f264f806c42cf3acf508ced71684a58cf57ebf88
Atabeig <effatiatabak1@gmail.com>
2025-11-21 23:24:00 +0330

    Add files via upload

---
+++ analytics.js
@@ -0,0 +1,41 @@
+export const dashboardStats = {
+  totalDeals: 256,
+  totalSavings: 48200000,
+  returningCustomers: 68,
+  newCustomers: 132,
+  period: '۱۴ آبان ۱۴۰۴',
+};
+
+export const salesTrend = [
+  { month: 'فروردین', value: 120 },
+  { month: 'اردیبهشت', value: 148 },
+  { month: 'خرداد', value: 175 },
+  { month: 'تیر', value: 163 },
+  { month: 'مرداد', value: 186 },
+  { month: 'شهریور', value: 198 },
+  { month: 'مهر', value: 212 },
+];
+
+export const topMerchants = [
+  {
+    id: 'merchant-1',
+    name: 'کافه سحرخیز',
+    category: 'غذا و نوشیدنی',
+    monthlyRevenue: 12800000,
+    satisfaction: 94,
+  },
+  {
+    id: 'merchant-2',
+    name: 'مرکز سلامتی سُما',
+    category: 'زیبایی و سلامت',
+    monthlyRevenue: 10400000,
+    satisfaction: 97,
+  },
+  {
+    id: 'merchant-3',
+    name: 'آکادمی پالس',
+    category: 'آموزش',
+    monthlyRevenue: 8600000,
+    satisfaction: 91,
+  },
+];
---
+++ categories.js
@@ -0,0 +1,37 @@
+export const topCategories = [
+  {
+    id: 'cat-1',
+    name: 'غذا و نوشیدنی',
+    icon: '🍽️',
+    activeDeals: 68,
+    trend: 12,
+  },
+  {
+    id: 'cat-2',
+    name: 'زیبایی و سلامت',
+    icon: '💆‍♀️',
+    activeDeals: 41,
+    trend: 8,
+  },
+  {
+    id: 'cat-3',
+    name: 'تفریح و سرگرمی',
+    icon: '🎉',
+    activeDeals: 23,
+    trend: -5,
+  },
+  {
+    id: 'cat-4',
+    name: 'آموزش',
+    icon: '📚',
+    activeDeals: 31,
+    trend: 4,
+  },
+  {
+    id: 'cat-5',
+    name: 'تکنولوژی و دیجیتال',
+    icon: '💻',
+    activeDeals: 17,
+    trend: 2,
+  },
+];
